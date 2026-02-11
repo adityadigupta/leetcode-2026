878d75148ec151f3a7a843cec8ee54db70be4851
adityadigupta <adityadigupta95@gmail.com>
2026-02-10 21:20:21 -0600

    Problem 14: Longest Common Prefix

--- leetcode/easy/string/LongestCommonPrefix.java
+++ leetcode/easy/string/LongestCommonPrefix.java
@@ -17,22 +17,20 @@ public class LongestCommonPrefix {
             0 <= strs[i].length <= 200
     strs[i] consists of only lowercase English letters if it is non-empty.*/
     public static void main(String[] args) {
-        String[] strs = {"cir","car"};
+        String[] strs = {"cir", "car"};
         System.out.println(longestCommonPrefix(strs));
     }
 
     public static String longestCommonPrefix(String[] strs) {
         Arrays.sort(strs);
-        int lastIndex = strs.length - 1;
-        int length1 = strs[lastIndex].length();
-        int length2 = strs[0].length();
-        int length = length2 < length1 ? length2 : length1;
+        String str1 = strs[0];
+        String str2 = strs[strs.length - 1];
         StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < length; i++) {
-            if (strs[0].charAt(i) == strs[lastIndex].charAt(i)) {
+        for (int i = 0; i < str1.length(); i++) {
+            if (str1.charAt(i) == str2.charAt(i)) {
                 stringBuilder.append(strs[0].charAt(i));
-            } else if (stringBuilder.isEmpty()) {
-                return stringBuilder.toString();
+            } else {
+                break;
             }
         }
         return stringBuilder.toString();
