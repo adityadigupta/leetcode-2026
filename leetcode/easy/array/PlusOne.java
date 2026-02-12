356d4c3fd57ef173e8cfc955b099b1d6f718991a
adityadigupta <adityadigupta95@gmail.com>
2026-02-12 15:23:45 -0600

    Problem 66. Plus One

--- leetcode/easy/array/PlusOne.java
+++ leetcode/easy/array/PlusOne.java
@@ -19,6 +19,7 @@ public class PlusOne {
     }
 
     public static int[] plusOne(int[] digits) {
+
         for (int i = digits.length - 1; i >= 0; i--) {
             if (digits[i] < 9) {
                 digits[i]++;
