17ee19ac74c1d74cd6f9e83bc7859c621eb82845
adityadigupta <adityadigupta95@gmail.com>
2026-02-25 13:20:27 -0600

    13. Roman to Integer|| Timecomplexity O(N)

---
+++ leetcode/easy/string/RomanToInteger.java
@@ -0,0 +1,55 @@
+package leetcode.easy.string;
+
+import java.util.HashMap;
+import java.util.Hashtable;
+
+/*Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II.
+The number 27 is written as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to right. However,
+the numeral for four is not IIII. Instead, the number four is written as IV.
+Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX.
+There are six instances where subtraction is used:
+
+I can be placed before V (5) and X (10) to make 4 and 9.
+X can be placed before L (50) and C (100) to make 40 and 90.
+C can be placed before D (500) and M (1000) to make 400 and 900.
+Given a roman numeral, convert it to an integer.*/
+public class RomanToInteger {
+
+    public static void main(String[] args) {
+
+        System.out.println(romanToInt("LVIII"));
+    }
+    public static int romanToInt(String s) {
+        HashMap<Character, Integer> romanToNumber = new HashMap<>();
+        romanToNumber.put('I', 1);
+        romanToNumber.put('V', 5);
+        romanToNumber.put('X', 10);
+        romanToNumber.put('L', 50);
+        romanToNumber.put('C', 100);
+        romanToNumber.put('D', 500);
+        romanToNumber.put('M', 1000);
+        int total=0;
+        int prev = 0;
+        for (int i = s.length()  -1; i >= 0; i--) {
+            int curr=romanToNumber.get(s.charAt(i));
+            if(curr<prev){
+                total-=curr;
+            }else{
+                total+=curr;
+            }
+            prev=curr;
+        }
+        return total;
+    }
+}
